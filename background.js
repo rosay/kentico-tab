f407e7c6a1f5b75bc061c28dfb4dd48518e209b5
Cody Rose <crose@bizstream.com>
2013-02-27 09:17:38 -0500

    Changed getHostname method to return hostname and port. Also cleaned up some other code.

--- background.js
+++ background.js
@@ -1,24 +1,19 @@
 chrome.commands.onCommand.addListener(function(command) {
-    console.log(command);
+    // TODO verify that it's a Kentico site.
+    
     chrome.tabs.getSelected(null, function(tab) {
-        ktOnClick(command, tab)
+        openTab(command, tab)
     });
 });
 
 var getHostname = function(href) {
+    // stack exchange love - http://stackoverflow.com/questions/736513/how-do-i-parse-a-url-into-hostname-and-path-in-javascript
     var l = document.createElement("a");
     l.href = href;
-    return l.hostname;
+    return l.host; // Host returns hostname and port if there is one.
 };
 
-// Create context menus - not needed because the point of the plugin is to make keyboard shortcuts.
-//cmsdesk = chrome.contextMenus.create({"title": "Go to CMS Desk", "onclick": ktOnClick, "id": "cmsdesk" });
-//cmssiteman = chrome.contextMenus.create({"title": "Go to Site Manager", "onclick": ktOnClick, "id": "cmssitemanager"});
-
-function ktOnClick(pCommand, pTab) {
-
-    // TODO verify that it's a Kentico site.
-    
+function openTab(pCommand, pTab) {
     var domain = getHostname(pTab.url.toString());
     url = "http://" + domain + "/" + pCommand
     
